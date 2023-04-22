8459ea18cbd96ae74c0ab056bf2b39f1bb23054b
Angelosz <angelcabgil@gmail.com>
2023-04-22 18:10:12 +0100

    Extracts PieceBuilder from Piece.

--- src/main/kotlin/board/piece/Piece.kt
+++ src/main/kotlin/board/piece/Piece.kt
@@ -3,20 +3,6 @@ package board.piece
 import Player
 import character.*
 
-object PieceBuilder {
-  fun build(pieceName: String): Piece
-  {
-    return when(pieceName.first()){
-      'T' -> TreePiece()
-      'C' -> ChestPiece(LootTable.chests[pieceName.removePrefix("C").toInt()])
-      'E' -> EnemyPiece(LootTable.enemies[pieceName.removePrefix("E").toInt()])
-      'B' -> BossPiece()
-      '<' -> TownPiece()
-      else -> EmptyPiece()
-    }
-  }
-}
-
 sealed interface Piece{
   val display: String
   val info: String
